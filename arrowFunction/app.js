3ab90a12e164d9c04654b290ecea70e0ddd1a8c5
Çağatay Palabıyık <98713471+Clabiyik@users.noreply.github.com>
2023-02-11 16:14:20 +0300

    arrowFunction

---
+++ arrowFunction/app.js
@@ -0,0 +1,31 @@
+// const Hi= function(){
+//     console.log("Hi");
+// }
+// Hi();
+
+
+
+
+//*******ARROW FUNCTION************
+
+
+// const Hi= ()=>{
+//     console.log("Hi");
+// }
+// Hi();
+
+
+// const Hi=(firstName,lastName)=>{
+//     console.log("Hi",firstName,lastName)
+// }
+// Hi("Çağatay","Palabiyik");
+
+
+
+// const cube=function(x){
+//     return x*x*x;
+// }
+// console.log(cube(4));
+
+const cube=x =>x*x*x;
+console.log(cube(4));
