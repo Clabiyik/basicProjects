683ae06d342186bdc89b4dca3420753daf5ae61d
Çağatay Palabıyık <98713471+Clabiyik@users.noreply.github.com>
2023-02-15 00:18:43 +0300

    arrowFunction-This

---
+++ ArrowFunction-This/app.js
@@ -0,0 +1,15 @@
+const person={
+    age:25,
+    // tellAge:function(){
+    //     //this-Person
+    //     console.log(this.age);
+    // }.bind(this)
+    tellAge:()=>{
+        console.log(this);
+        console.log(this.age);
+    }
+
+
+    //this-Window
+}
+person.tellAge();
